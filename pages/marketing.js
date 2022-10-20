81f9ee0ebd5e4ed973b7e620df76125cb79c4242
shadid12 <shadidhaque2012@gmail.com>
2022-10-19 22:41:49 -0400

    marketing page added

---
+++ pages/marketing.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Marketing() {
+  return (
+    <div>
+      <h1>Marketing Page</h1>
+      <Link href="/"><a>Home</a></Link>
+    </div>
+  )
+}
