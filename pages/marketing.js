64ca6aa3b6b03b33cc91ee9bb21da0062f88d33d
shadid12 <shadidhaque2012@gmail.com>
2022-10-19 23:11:01 -0400

    main logic for middleware

--- pages/marketing.js
+++ pages/marketing.js
@@ -1,9 +1,19 @@
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
-export default function Marketing() {
+export async function getStaticProps() {
+  return {
+    props: {
+      message: "This is a static page — and a prop",
+    },
+  };
+}
+
+export default function Marketing({message}) {
   return (
     <div>
       <h1>Marketing Page</h1>
+      <p id="message">{message}</p>
       <Link href="/"><a>Home</a></Link>
     </div>
   )
